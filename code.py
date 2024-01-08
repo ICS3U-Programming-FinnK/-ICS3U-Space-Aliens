4dde90fb9b0e124730f5cd9418060d5a70b9ae66
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2024-01-08 13:06:28 -0500

    Edited code.py

---
+++ code.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Created by: Finn Kitor
+# Created on: January 8th, 2023
+# This program is a space fighter game
+
+import ugame
+import stage
+
+# this function says "Hello, my name, and happy new year"
+
+def game_scene():
+
+
+# prints 3 blank spaces
+    print ("\n\n\n")
+# prints my name
+    print("Hello, Finn!")
+# prints happy new year
+    print("Happy new year!")
+
+    #game loop
+    while True:
+        pass #for now, this is a placeholder
+
+    if __name__ == "__main__":
+        game_scene()
