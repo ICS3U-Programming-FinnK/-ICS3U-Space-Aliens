06f7ae4d4adec9729f608719e2ab4d45802b40f8
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2024-01-09 12:48:37 -0500

    Added background image to code.py

--- code.py
+++ code.py
@@ -8,20 +8,20 @@ import stage
 
 def game_scene():
 
-# the image bank for CircuitPython
-image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    # the image bank for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
 
-# set background to image 0 in the image bank and size (10x8 tiles of size 16x16)
-background = stage.Grid(image_bank_background, 10, 8)
+    # set background to image 0 in the image bank and size (10x8 tiles of size 16x16)
+    background = stage.Grid(image_bank_background, 10, 8)
 
-# creating a stage for the background to show up on and set fps to 60
-game = stage.Stage(ugame.display, 60)
+    # creating a stage for the background to show up on and set fps to 60
+    game = stage.Stage(ugame.display, 60)
 
-# set layers of sprites, they will show up in order
-game.layers = [background]
+    # set layers of sprites, they will show up in order
+    game.layers = [background]
 
-# render the sprites
-game.render_block()
+    # render the sprites
+    game.render_block()
 
     # game loop
     while True:
