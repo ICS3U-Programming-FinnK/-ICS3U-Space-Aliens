a941075d6b0e4f19d750837e679ebf49b383025b
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2024-01-09 12:33:41 -0500

    Edited code.py

--- code.py
+++ code.py
@@ -6,21 +6,28 @@
 import ugame
 import stage
 
-# this function says "Hello, my name, and happy new year"
-
+# this function is the main game (game_scene)
 def game_scene():
 
+# the image bank for CircuitPython
+image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
 
-# prints 3 blank spaces
-    print ("\n\n\n")
-# prints my name
-    print("Hello, Finn!")
-# prints happy new year
-    print("Happy new year!")
+# set background to image 0 in the image bank and size (10x8 tiles of size 16x16)
+background = stage.Grid(image_bank_background, 10, 8)
 
-    #game loop
+# creating a stage for the background to show up on and set fps to 60
+game = stage.Stage(ugame.display, 60)
+
+# set layers of sprites, they will show up in order
+game.layers = [background]
+
+# render the sprites
+game.render_block()
+
+    # game loop
     while True:
-        pass #for now, this is a placeholder
+        pass # for now, this is a placeholder
 
-    if __name__ == "__main__":
-        game_scene()
+if __name__ == "__main__":
+    game_scene()
+    
