3eb35677164075775ee7c1b97b524c4d0fd9e4ab
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2024-01-09 13:04:38 -0500

    Added a sprite to code.py

--- code.py
+++ code.py
@@ -8,11 +8,15 @@ import stage
 
 def game_scene():
 
-    # the image bank for CircuitPython
+    # the image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
     # set background to image 0 in the image bank and size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
+
+    # a sprite that will be updated every frame
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
     # creating a stage for the background to show up on and set fps to 60
     game = stage.Stage(ugame.display, 60)
