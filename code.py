a919d64a802083405434004c86672d3a90cb51de
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2024-01-10 12:09:10 -0500

    Edited code.py

--- code.py
+++ code.py
@@ -52,7 +52,7 @@ def game_scene():
         # update game logic
 
         # redraw the sprites
-        game.render_sprited([ship])
+        game.render_sprites([ship])
         game.tick()
 
 if __name__ == "__main__":
